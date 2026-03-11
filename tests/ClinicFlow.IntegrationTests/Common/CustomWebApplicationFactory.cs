df65d917a17ee71c926aaf7aed5a2d38c532d14e
Enes Garip <enes98.garip@gmail.com>
2026-03-12 00:03:14 +0300

    yml update & bug fix

--- tests/ClinicFlow.IntegrationTests/Common/CustomWebApplicationFactory.cs
+++ tests/ClinicFlow.IntegrationTests/Common/CustomWebApplicationFactory.cs
@@ -1,41 +1,18 @@
-﻿using ClinicFlow.Infrastructure.Persistence;
-using Microsoft.AspNetCore.Hosting;
+﻿using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace ClinicFlow.IntegrationTests;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseEnvironment("Testing");
-
-        builder.ConfigureServices(services =>
-        {
-            services.RemoveAll<DbContextOptions<ClinicFlowDbContext>>();
-            services.RemoveAll<ClinicFlowDbContext>();
-
-            var connectionString =
-                "Host=localhost;Port=5432;Database=clinicflowdb;Username=postgres;Password=postgres";
-
-            services.AddDbContext<ClinicFlowDbContext>(options =>
-                options.UseNpgsql(connectionString));
-
-            var sp = services.BuildServiceProvider();
-
-            using var scope = sp.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<ClinicFlowDbContext>();
-
-            db.Database.EnsureCreated();
-        });
-    }
-    public HttpClient CreateTenantClient()
+    public HttpClient CreateTenantClient(Guid? tenantId = null)
     {
         var client = CreateClient();
-        client.DefaultRequestHeaders.Add("X-Tenant-Id", "11111111-1111-1111-1111-111111111111");
+
+        client.DefaultRequestHeaders.Add(
+            "X-Tenant-Id",
+            (tenantId ?? Guid.Parse("11111111-1111-1111-1111-111111111111")).ToString());
+
         return client;
     }
 }
